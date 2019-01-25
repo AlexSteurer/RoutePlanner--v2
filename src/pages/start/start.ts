6f56997171c13cc41e871f4f44a0c4ac294070d4
Fahri MacBook <fahri-can@web.de>
2019-01-25 20:57:02 +0100

    Split to long methods into separate and add comments.

--- src/pages/start/start.ts
+++ src/pages/start/start.ts
@@ -18,10 +18,8 @@ import {StyledMap} from "../../model/styledMap";
 import {TodoPage} from "../todo/todo";
 import {ModalinfoPage} from "../modalinfo/modalinfo";
 
-
+//interface to Google Maps API
 declare let google;
-let infoWindow: any;
-let service: any;
 
 
 @IonicPage()
@@ -71,12 +69,10 @@ export class StartPage {
                 private alertCtrl: AlertController,
                 public clientsProvider: ClientsProvider,
                 private toastCtrl: ToastController,
-                public events: Events, public modalCtrl: ModalController,) {
+                public events: Events, public modalCtrl: ModalController) {
 
         this.todayDateObj = new Date();
         this.navCtrl = navCtrl;
-       // events.subscribe('client:deleted', client => this.loadMap());
-
         this.GoogleAutocomplete = new google.maps.places.AutocompleteService();
         this.autocomplete = {input: ''};
         this.autocompleteItems = [];
@@ -86,12 +82,10 @@ export class StartPage {
     }
 
     ionViewDidLoad() {
-        //Alternative Searchbox von Google - aktuell nicht verwendet !
-        /*  let elem = <HTMLInputElement>document.getElementsByClassName('searchbar-input')[0];
-         this.autocomplete = new google.maps.places.SearchBox(elem); */
         this.loadMap();
     }
 
+    //Saves the client as document in Cloud Firestore and refresh of the map.
     saveClient() {
         this.clientsProvider.add(this.client).then(res => {
             let toast = this.toastCtrl.create({
@@ -103,7 +97,7 @@ export class StartPage {
         })
     }
 
-    updateSearchResults() {
+    checkInputForSearchField() {
         if (this.autocomplete.input === '') {
             this.autocompleteItems = [];
             return;
@@ -116,8 +110,6 @@ export class StartPage {
      * @param item suggested address from search bar
      */
     selectSearchResult(item) {
-        let time_chosen : null;
-        let time_half :null ;
         let icon = 'http://maps.google.com/mapfiles/ms/icons/blue-dot.png';
         this.markers = [];
         this.autocompleteItems = [];
@@ -125,10 +117,9 @@ export class StartPage {
             .geocode({'placeId': item.place_id}, (results, status) => {
                 if (status === 'OK' && results[0]) {
                     let address = this.setClientAttributes(results, item);
-                    let marker = this.createMarkerOnGoogleMaps(results, item.description,icon);
+                    let marker = this.createMarkerOnGoogleMaps(results, item.description, icon);
                     this.saveClient();
                     let infoWindow = this.createClientInfoWindow(address, this.client.title);
-                    console.log("doc id: ", item.id);
                     this.addListenerOnGoogleMaps(infoWindow, marker, this.client.placeId, item.id);
                     this.markers.push(marker);
                     this.map.setCenter(results[0].geometry.location);
@@ -138,12 +129,6 @@ export class StartPage {
     }
 
     redirectToTasks() {
-        /*let modalCtrl = this.modalCtrl;
-        let modal = modalCtrl.create(TasksPage);
-        modal.present()
-            .then(val => console.log("redirectToTasks success"))
-            .catch(err => console.log("redirectToTasks error: ", err.error));
-        this.loadMap();*/
         this.navCtrl.push(TodoPage, {
             theDocId: StartPage.theDocId
         });
@@ -156,7 +141,8 @@ export class StartPage {
         this.afAuth.authState.subscribe(user => {
                 if (user) {
                     this.userId = user.uid;
-                    this.db.collection(user.uid).where("placeId", "==", placeId)
+                    this.db.collection(user.uid)
+                        .where("placeId", "==", placeId)
                         .get()
                         .then(querySnapshot => {
                             querySnapshot
@@ -165,77 +151,71 @@ export class StartPage {
                         .catch(err => console.log("markerLoad error: " + err.error))
                 }
             }
-        )
+        );
         this.redirectToInfoModal();
     };
 
     createListMarkers() {
-        let icon: string;
+        let colorMapsMarker: string;
         this.afAuth.authState.subscribe(user => {
             if (user) {
                 this.userId = user.uid;
                 this.db.collection(user.uid).get().then(docs => {
                     docs.forEach(coordinate => {
-                        console.log('coordinate id:', coordinate.id);
-                        const time_chosen = coordinate.data().time_chosen;
-                        const time_half = coordinate.data().time_half;
-
-                        if(time_chosen == null && time_half == null){
-
-                            icon = 'http://maps.google.com/mapfiles/ms/icons/blue-dot.png';
-                        }
-                        else{
-                            icon = this.changeMarkerColor(time_chosen,time_half);
-                        }
-
-                        const title = coordinate.data().title;
-                        const address = coordinate.data().adress;
-                        const placeId = coordinate.data().placeId;
-                        const position = new google.maps.LatLng(coordinate.data().location._lat, coordinate.data().location._long);
-                        const marker = this.createMarkerOnGoogleMaps(position, title, icon);
-                        const infoWindow = this.createClientInfoWindow(address, title);
-                        this.addListenerOnGoogleMaps(infoWindow, marker, placeId, coordinate.id);
+                        this.fillDataForMarker(coordinate, colorMapsMarker);
                     })
                 })
             }
         });
     }
+
     /**
-     * Sets a marker on maps when user clicks on 'Add Client'.
-     * @param item suggested address from search bar
+     * All necessary data like title or address, placeID, position gets set
+     * @param coordinate is the document from Cloud Firestore
+     * @param colorMapsMarker color of the marker can only be blue, yellow or red
      */
-    changeMarkerColor(time_chosen,time_half) {
+    private fillDataForMarker(coordinate, colorMapsMarker: string) {
+        const time_chosen = coordinate.data().time_chosen;
+        const time_half = coordinate.data().time_half;
+        if (time_chosen == null && time_half == null) {
+            colorMapsMarker = 'http://maps.google.com/mapfiles/ms/icons/blue-dot.png';
+        } else {
+            colorMapsMarker = this.changeMarkerColor(time_chosen, time_half);
+        }
+        const title = coordinate.data().title;
+        const address = coordinate.data().adress;
+        const placeId = coordinate.data().placeId;
+        const position = new google.maps.LatLng(coordinate.data().location._lat, coordinate.data().location._long);
+        const marker = this.createMarkerOnGoogleMaps(position, title, colorMapsMarker);
+        const infoWindow = this.createClientInfoWindow(address, title);
+        this.addListenerOnGoogleMaps(infoWindow, marker, placeId, coordinate.id);
+    }
+
+    /**
+     *
+     * @param time_chosen
+     * @param time_half
+     */
+    changeMarkerColor(time_chosen, time_half) {
 
         let icon: string;
         const chosen = time_chosen;
         const half = time_half;
         const now = moment();
 
-        console.log(now);
-        console.log(chosen);
-        console.log(half);
+        if (now.isAfter(moment.unix(chosen.seconds))) {
+            icon = 'http://maps.google.com/mapfiles/ms/icons/red-dot.png';
+            return icon;
+        }
+        if (now.isAfter(moment.unix(half.seconds)) && now.isBefore(moment.unix(chosen.seconds))) {
+            icon = 'http://maps.google.com/mapfiles/ms/icons/yellow-dot.png';
+            return icon;
 
-
-            if(now.isAfter(moment.unix(chosen.seconds))){
-                icon = 'http://maps.google.com/mapfiles/ms/icons/red-dot.png';
-                console.log(icon);
-                return icon;
-            }
-            if(now.isAfter(moment.unix(half.seconds)) && now.isBefore(moment.unix(chosen.seconds))){
-                icon = 'http://maps.google.com/mapfiles/ms/icons/yellow-dot.png';
-                console.log(icon);
-                return icon;
-
-            }
-
-            else {
-                icon = 'http://maps.google.com/mapfiles/ms/icons/blue-dot.png';
-                console.log(icon);
-                return icon;
-            }
-
+        } else {
+            icon = 'http://maps.google.com/mapfiles/ms/icons/blue-dot.png';
+            return icon;
+        }
     }
-        
 
 
     /**
@@ -246,17 +226,16 @@ export class StartPage {
      */
     private createMarkerOnGoogleMaps(position, title, icon) {
 
-            return new google.maps.Marker({
-                position,
-                map: this.map,
-                icon: icon,
-                title: title,
-                animation: google.maps.Animation.DROP,
-            });
+        return new google.maps.Marker({
+            position,
+            map: this.map,
+            icon: icon,
+            title: title,
+            animation: google.maps.Animation.DROP,
+        });
 
 
-
-        }
+    }
 
 
     loadMap() {
@@ -322,7 +301,7 @@ export class StartPage {
             content: '<div><strong>' + title + '</strong><br/>'
                 + 'Address: ' + address + '<br/>'
                 + '</div>' + '<a id="myid"><strong>Show Client Task </strong></a>'
-            +'<hr/>'+'</div>' + '<a id="modalid"><strong>Show Client Info </strong></a>',
+                + '<hr/>' + '</div>' + '<a id="modalid"><strong>Show Client Info </strong></a>',
             maxWidth: 300
         });
     }
@@ -379,7 +358,7 @@ export class StartPage {
                 position: google.maps.ControlPosition.RIGHT_BOTTOM
             }
         };
-       return mapOptions
+        return mapOptions
     }
 
 
@@ -388,51 +367,8 @@ export class StartPage {
         //this.navCtrl.push(ModalinfoPage);
         let modal = this.modalCtrl.create(ModalinfoPage);
         modal.present()
-           .then(val => console.log("ModalInfoWindow success"))
-           .catch(err => console.log("ModalInfoWindow error: ", err.error));
+            .then(val => console.log("ModalInfoWindow success"))
+            .catch(err => console.log("ModalInfoWindow error: ", err.error));
 
     }
 }
-
-// Nearby Search - Currently not used in App !!!
-/* 
-  searchMarker() {
-    infoWindow = new google.maps.InfoWindow();
-    var service = new google.maps.places.PlacesService(this.map);
-    
-    this.geolocation.getCurrentPosition().then((position) => {
-    service.nearbySearch({
-      
-      location: {lat: position.coords.latitude, lng: position.coords.longitude}, 
-      radius: 1000,
-      type: ['store']
-    }, (results,status) => {
-      if (status === google.maps.places.PlacesServiceStatus.OK) {
-        for (var i = 0; i < results.length; i++) {
-          this.createMarker(results[i]);
-        }
-      }
-    });})
-  }
-
-    private createMarkerOnGoogleMaps2(results, item) {
-        return new google.maps.Marker({
-            map: this.map,
-            position: results[0].geometry.location,
-            animation: google.maps.Animation.DROP,
-            title: item.description,
-            icon: 'http://maps.google.com/mapfiles/ms/icons/blue-dot.png'
-        });
-    }
-
-*/
-
-
- 
-
-
-
-
-
-
-
