c4574184b89c1cbde1818fcec7e912234ff00b21
Fahri MacBook <fahri-can@web.de>
2019-01-28 20:45:06 +0100

    Refactor modal info page.

--- src/pages/modalinfo/modalinfo.ts
+++ src/pages/modalinfo/modalinfo.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams, ModalController } from 'ionic-angular';
+import {Component} from '@angular/core';
+import {IonicPage, NavController, NavParams, ModalController} from 'ionic-angular';
 import {ClientsProvider} from './../../providers/clients/clients';
 import {ViewController} from 'ionic-angular/navigation/view-controller';
 
@@ -16,48 +16,47 @@ import {Observable} from 'rxjs';
 
 @IonicPage()
 @Component({
-  selector: 'page-modalinfo',
-  templateUrl: 'modalinfo.html',
+    selector: 'page-modalinfo',
+    templateUrl: 'modalinfo.html',
 })
 export class ModalinfoPage {
 
-  private clientCollection: AngularFirestoreCollection<any>;
-  clientList: Observable<any[]>;
-  userId: String;
-  updateId = null;
-  public infoText: String;
-  public day_interval: number;
-  public new_date: Date;
-  public day_interval_half: Date;
-  public currentDate: Date;
+    private clientCollection: AngularFirestoreCollection<any>;
+    clientList: Observable<any[]>;
+    userId: String;
+    updateId = null;
+    public infoText: String;
+    public day_interval: number;
+    public new_date: Date;
+    public day_interval_half: Date;
+    public currentDate: Date;
 
-  clientData = {
-    title: null,
-    address: null,
-    location: null,
-    id: null,
-    info: null,
-    timestamp: null,
-    bool: false,
-    docId: String,
-    interval: null,
-  };
+    clientData = {
+        title: null,
+        address: null,
+        location: null,
+        id: null,
+        info: null,
+        timestamp: null,
+        bool: false,
+        docId: String,
+        interval: null,
+    };
 
-  constructor(private afs: AngularFirestore, public navCtrl: NavController,
-              public navParams: NavParams, public viewCtrl: ViewController,
-              public clientsProvider: ClientsProvider,
-              public modalController: ModalController,
-              private afAuth: AngularFireAuth) {
+    constructor(private afs: AngularFirestore, public navCtrl: NavController,
+                public navParams: NavParams, public viewCtrl: ViewController,
+                public clientsProvider: ClientsProvider,
+                public modalController: ModalController,
+                private afAuth: AngularFireAuth) {
 
-    this.currentDate = new Date();
-  }
+        this.currentDate = new Date();
+    }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad ModalinfoPage');
-  }
+    ionViewDidLoad() {
+    }
 
-  closeModal() {
-    this.viewCtrl.dismiss();
-  }
+    closeModal() {
+        this.viewCtrl.dismiss();
+    }
 
 }
